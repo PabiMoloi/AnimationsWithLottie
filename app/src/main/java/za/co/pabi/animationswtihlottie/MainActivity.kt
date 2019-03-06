b20a168b9b2103ad36a787cb695abb34d73997e9
Pabi Moloi <moloipaballo@gmail.com>
2019-03-06 07:06:26 +0200

    Fingerprint lottie animation load

--- app/src/main/java/za/co/pabi/animationswtihlottie/MainActivity.kt
+++ app/src/main/java/za/co/pabi/animationswtihlottie/MainActivity.kt
@@ -1,6 +1,6 @@
 package za.co.pabi.animationswtihlottie
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
